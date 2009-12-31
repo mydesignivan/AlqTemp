1f58003b9ab850ceb3d1659e04e1df090d79a368
Ivan Mattoni <ivan@master.(none)>
2009-12-31 14:22:57 -0300

    Nuevos cambios

--- index.php
+++ index.php
@@ -1,124 +1,118 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Alquileres temporarios</title>
-<link href="styles/style.css" rel="stylesheet" type="text/css" />
-<!--[if IE]>
-<link href="styles/styleIE.css" rel="stylesheet" type="text/css" />
-<![endif]-->
-    
-  	<script type="text/JavaScript" src="js/curvycorners.js"></script>
-    <script type="text/JavaScript">
+<?php
+/*
+|---------------------------------------------------------------
+| PHP ERROR REPORTING LEVEL
+|---------------------------------------------------------------
+|
+| By default CI runs with error reporting set to ALL.  For security
+| reasons you are encouraged to change this when your site goes live.
+| For more info visit:  http://www.php.net/error_reporting
+|
+*/
+	error_reporting(E_ALL);
 
-  addEvent(window, 'load', initCorners);
+/*
+|---------------------------------------------------------------
+| SYSTEM FOLDER NAME
+|---------------------------------------------------------------
+|
+| This variable must contain the name of your "system" folder.
+| Include the path if the folder is not in the same  directory
+| as this file.
+|
+| NO TRAILING SLASH!
+|
+*/
+	$system_folder = "system";
 
-  function initCorners() {
-    var settings = {
-      tl: { radius: 20 },
-      tr: { radius: 0 },
-      bl: { radius: 20 },
-      br: { radius: 0 },
-      antiAlias: true
-    }
-	curvyCorners(settings, "#mainContent,.mainContent2");
-  }
-  
-</script>
-</head>
+/*
+|---------------------------------------------------------------
+| APPLICATION FOLDER NAME
+|---------------------------------------------------------------
+|
+| If you want this front controller to use a different "application"
+| folder then the default one you can set its name here. The folder 
+| can also be renamed or relocated anywhere on your server.
+| For more info please see the user guide:
+| http://codeigniter.com/user_guide/general/managing_apps.html
+|
+|
+| NO TRAILING SLASH!
+|
+*/
+	$application_folder = "application";
 
-<body>
+/*
+|===============================================================
+| END OF USER CONFIGURABLE SETTINGS
+|===============================================================
+*/
 
-    <div id="container">
-      <div id="header">
-      	<?php include ('includes/header.php');?>
-      </div><!-- end #header -->
-      
-      <div id="sidebar1">
-      	<div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>
-        <div class="publicity_horizontal">publicidad</div>        
-      </div><!-- end #sidebar1 -->
-      
-      <div class="container_mainContent">
-            <div id="mainContent">
-                <div class="content_top">
-                  <h1>Alquileres Destacados</h1>
-                </div>
-                <div class="description_properties">
-                    <div class="image_properties"><img src="images/image_properties1.png" /></div>
-                    <div class="description_text"><h2>San Lorenzo Apartments</h2>
-                        <p>Ajkljlkjeqw jlkjadk wqe jklasd lkjalsdjasd klñkñalskd alkasd jka dasdas.</p>
-                        <b>Categor&iacute;a:</b> Departamentos <br />
-                        <b>Ciudad:</b> Mendoza 
-                        <span><br />Precio: $250</span><a class="info" href="#">Mas info</a></p>
-                    </div>
-                </div>
-            
-                <div class="description_properties">
-                    <div class="image_properties"><img src="images/image_properties1.png" /></div>
-                    <div class="description_text"><h2>San Lorenzo Apartments</h2>
-                        <p>Ajkljlkjeqw jlkjadk wqe jklasd lkjalsdjasd klñkñalskd alkasd jka dasdas.</p>
-                        <b>Categor&iacute;a:</b> Departamentos <br />
-                        <b>Ciudad:</b> Mendoza 
-                      <span><br />Precio: $250</span><a class="info" href="#">Mas info</a></p>
-                    </div>
-                </div>
-                
-                <div class="description_properties">
-                    <div class="image_properties"><img src="images/image_properties1.png" /></div>
-                    <div class="description_text"><h2>San Lorenzo Apartments</h2>
-                        <p>Ajkljlkjeqw jlkjadk wqe jklasd lkjalsdjasd klñkñalskd alkasd jka dasdas.</p>
-                        <b>Categor&iacute;a:</b> Departamentos <br />
-                        <b>Ciudad:</b> Mendoza 
-                        <span><br />Precio: $250</span><a class="info" href="#">Mas info</a></p>
-                    </div>
-                </div>
-          </div><!-- end #mainContent -->
-      
-      
-    	<!-- Este elemento de eliminación siempre debe ir inmediatamente después del div #mainContent para forzar al div #container a que contenga todos los elementos flotantes hijos --><br class="clearfloat" />
-        
-            <div class="mainContent2">
-                <div class="content_top"><h1>Destinos mas Buscados</h1></div>
-                <div class="destinations">
-                    <div class="column line_right">
-                        <ul>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        </ul>
-                    </div>
-                    <div class="column line_right">
-                        <ul>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        </ul>
-                    </div>
-                    <div class="column">
-                        <ul>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        <li>Mar del Plata</li>
-                        </ul>
-                    </div>
-                </div>
-                <h2>&nbsp;</h2>
-            </div>
-      </div> <!--end .container_mainContent-->
-      
-      
-      <div id="footer">
-      <?php include ('includes/footer.php');?>
-      </div><!-- end #footer -->
-    </div><!-- end #container -->
-    </body>
-</html>
+
+/*
+|---------------------------------------------------------------
+| SET THE SERVER PATH
+|---------------------------------------------------------------
+|
+| Let's attempt to determine the full-server path to the "system"
+| folder in order to reduce the possibility of path problems.
+| Note: We only attempt this if the user hasn't specified a 
+| full server path.
+|
+*/
+if (strpos($system_folder, '/') === FALSE)
+{
+	if (function_exists('realpath') AND @realpath(dirname(__FILE__)) !== FALSE)
+	{
+		$system_folder = realpath(dirname(__FILE__)).'/'.$system_folder;
+	}
+}
+else
+{
+	// Swap directory separators to Unix style for consistency
+	$system_folder = str_replace("\\", "/", $system_folder); 
+}
+
+/*
+|---------------------------------------------------------------
+| DEFINE APPLICATION CONSTANTS
+|---------------------------------------------------------------
+|
+| EXT		- The file extension.  Typically ".php"
+| SELF		- The name of THIS file (typically "index.php")
+| FCPATH	- The full server path to THIS file
+| BASEPATH	- The full server path to the "system" folder
+| APPPATH	- The full server path to the "application" folder
+|
+*/
+define('EXT', '.php');
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+define('FCPATH', str_replace(SELF, '', __FILE__));
+define('BASEPATH', $system_folder.'/');
+
+if (is_dir($application_folder))
+{
+	define('APPPATH', $application_folder.'/');
+}
+else
+{
+	if ($application_folder == '')
+	{
+		$application_folder = 'application';
+	}
+
+	define('APPPATH', BASEPATH.$application_folder.'/');
+}
+
+/*
+|---------------------------------------------------------------
+| LOAD THE FRONT CONTROLLER
+|---------------------------------------------------------------
+|
+| And away we go...
+|
+*/
+require_once BASEPATH.'codeigniter/CodeIgniter'.EXT;
+
+/* End of file index.php */
+/* Location: ./index.php */
