2b8c77ff1231d8cbeb415f9be7aa9077ccbaa814
Ivan Mattoni <ivan@Lusi.(none)>
2010-03-14 23:38:50 -0300

    nuevos cambios2

---
+++ application/logs/log-2010-03-14.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 14-03-2010 01:41:16 --> Severity: Notice  --> Undefined index: txtSearch /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 79
+ERROR - 14-03-2010 01:41:16 --> Severity: Notice  --> Undefined index: cboCountry /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 83
+ERROR - 14-03-2010 01:41:16 --> Severity: Notice  --> Undefined index: cboStates /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 85
+ERROR - 14-03-2010 01:41:16 --> Severity: Notice  --> Undefined index: cboCity /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 88
+ERROR - 14-03-2010 01:41:16 --> Severity: Notice  --> Undefined index: cboCategory /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 90
+ERROR - 14-03-2010 21:16:15 --> Severity: Notice  --> Undefined index: txtSearch /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 79
+ERROR - 14-03-2010 21:16:15 --> Severity: Notice  --> Undefined index: cboCountry /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 83
+ERROR - 14-03-2010 21:16:15 --> Severity: Notice  --> Undefined index: cboStates /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 85
+ERROR - 14-03-2010 21:16:15 --> Severity: Notice  --> Undefined index: cboCity /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 88
+ERROR - 14-03-2010 21:16:15 --> Severity: Notice  --> Undefined index: cboCategory /media/datos/Proyectos/PHP/trabajos/AlqTemp.git/application/views/includes/header_inc.php 90
